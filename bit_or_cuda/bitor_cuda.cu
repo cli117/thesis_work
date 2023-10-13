9d2524d511744c9b3c0b468abd513c03503d0ef9
cli117 <cheng.li2@mail.mcgill.ca>
2023-10-13 12:34:28 -0400

    shiftor cuda finished

--- bit_or_cuda/bitor_cuda.cu
+++ bit_or_cuda/bitor_cuda.cu
@@ -1,14 +1,48 @@
 #include "stdio.h"
 
-__global__ void build_mask_table(char* pattern, int pattern_length, int** table)
+
+// Here we set the maximum length of pattern_length is 32
+__global__ void build_mask_table(char* pattern, int pattern_length, unsigned long long* mask_table)
 {
     int char_idx = blockIdx.x;
     int pat_idx = threadIdx.x;
-    if (char_idx > 255 || pat_idx >= pattern_length)
-    {
-        return;
-    }
-    
-    table[char_idx][pat_idx] = int(pattern[pat_idx]) != char_idx;
+
+    atomicAnd(&mask_table[char_idx], ~((int(pattern[pat_idx]) == char_idx) << (pattern_length - pat_idx - 1)));
     return;
+}
+
+__global__ void shift_or(char** string_cuda, int* packets_size_config, unsigned long long* mask_table, int pat_len)
+{
+    char* string = string_cuda[blockIdx.x];
+    int str_len = packets_size_config[blockIdx.x];
+    int stride = blockDim.x;
+    __shared__ unsigned long long prev;
+    __shared__ int found;
+
+    if (threadIdx.x == 0)
+    {
+        found = 0;
+        prev = ~0;
+    }
+    __syncthreads();
+
+    int curr_pos = threadIdx.x;
+    unsigned long long curr_mask;
+    while (!found && curr_pos < str_len)
+    {
+        curr_mask = prev >> (65 - pat_len);
+        __syncthreads();
+        prev = 0;
+        __syncthreads();
+        curr_mask |= mask_table[string[curr_pos]] << threadIdx.x;
+        atomicOr(&prev, curr_mask);
+        __syncthreads();
+        if (!(prev & (1ULL << threadIdx.x)))
+        {
+            atomicAdd(&found, 1);
+            printf("found! at packet %d, %d\n", blockIdx.x, curr_pos);
+        }
+        __syncthreads();
+        curr_pos += stride;
+    }
 }
